a1671916fef3f7d87c739bb08afbeaac9ae667eb
msimpson27 <kenzlow11@gmail.com>
2024-05-13 19:31:59 -0400

    The actual program

---
+++ m.simpson_knockknock.py.py
@@ -0,0 +1,38 @@
+Name = input("What is your name? ")
+
+print (" Oh that's a nice name! ")
+
+feeling = input("how are you feeling today/night? ")
+
+if feeling == "bad":
+    print("Oh, I'm sorry to hear that!")
+    
+elif feeling == "tired":
+    print("Oh, I'm sorry to hear that!")
+else:
+    print("That's great to hear!")
+
+answer1 = input("Do you want to hear a joke? (y/n)")
+
+if answer1 == "yes":
+    input("Great! Knock Knock!")
+    input("Britney")
+    input("Knock Knock!")
+    print("Oops I did it again! XD")
+    exit()
+if answer1 == "no":
+    print("Oh, no!")
+
+answer2 = input("Are you sure? (y/n)")
+
+if answer2 == "yes":
+    print("Hmm... Darn, guess you won't hear this joke!")
+    exit()
+
+if answer2 == "no":
+    input("Great! Knock Knock!")
+        
+input("Britney")
+if knock == "Whos there":
+    input("Knock Knock!")
+print("Oops I did it again! XD")
